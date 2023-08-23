6b44f41a3386d05b41c53420f7fdc525042d66eb
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2023-08-23 10:27:27 +0100

    Sixth commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
-
-module.exports = {
-	images: {
-		domains: ['cdn.sanity.io']
-	}
-};
